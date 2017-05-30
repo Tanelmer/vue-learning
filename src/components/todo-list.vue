8289bc59f30c26b2d00617f1298cafe5e70eddf0
lixinyi <244lixinyi@163.com>
2017-05-30 21:28:10 +0800

    update to github

---
+++ src/components/todo-list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+    	<li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
